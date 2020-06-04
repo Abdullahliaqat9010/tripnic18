ed9233b2be83fbadaf5946f007075581ae3ad14f
Abdullahliaqat9010 <54941776+Abdullahliaqat9010@users.noreply.github.com>
2020-06-05 01:36:51 +0500

    Update react_firebase.js

--- src/auth/react_firebase.js
+++ src/auth/react_firebase.js
@@ -1,14 +1,14 @@
 import firebase from "@react-native-firebase/app";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAaKIsHkoQPEWT9CSATctpoJ-MXYDHKHhM",
-  authDomain: "tripnic-18.firebaseapp.com",
-  databaseURL: "https://tripnic-18.firebaseio.com",
-  projectId: "tripnic-18",
-  storageBucket: "tripnic-18.appspot.com",
-  messagingSenderId: "338070094498",
-  appId: "1:338070094498:web:e153e43e3027298b9e0dfb",
-  measurementId: "G-SW4EHGHGBX"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
